5af9aaeb987f54f8bfc927425de10cd077cff05e
Ilya Bastrakov <i.bastrakov.00@gmail.com>
2021-10-31 16:08:24 +0300

    Created window of game

--- desktop/src/ru/fadedfog/acrosstars/desktop/DesktopLauncher.java
+++ desktop/src/ru/fadedfog/acrosstars/desktop/DesktopLauncher.java
@@ -5,8 +5,12 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import ru.fadedfog.acrosstars.AcrosStartsGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main (String[] arg) { //TODO vars to config file
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "A c r o s s S t a r s";
+		config.width = 700;
+		config.height = 600;
+		config.resizable = false;
 		new LwjglApplication(new AcrosStartsGame(), config);
 	}
 }
