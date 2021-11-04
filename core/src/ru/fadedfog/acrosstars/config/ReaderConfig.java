b14f6820337b70cb348fbd9bd26b9588997fcb1e
Ilya Bastrakov <i.bastrakov.00@gmail.com>
2021-11-04 14:11:38 +0300

    Added dependency, created classes read/write for config-file

--- core/src/ru/fadedfog/acrosstars/config/ReaderConfig.java
+++ core/src/ru/fadedfog/acrosstars/config/ReaderConfig.java
@@ -1,5 +1,41 @@
 package ru.fadedfog.acrosstars.config;
 
-public class ReaderConfig {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+public class ReaderConfig {
+	private String pathFile;
+
+	public ReaderConfig(String pathFile) {
+		this.pathFile = pathFile;
+	}
+	
+	public Config read(GameConfig gameConfig) {
+		File jsonFile = new File(pathFile);
+		Config config = null;
+		try {
+			config = readConfig(jsonFile);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return config;
+	}
+	
+	private Config readConfig(File jsonFile) throws FileNotFoundException {
+		Gson gson = new Gson();
+		JsonReader reader = new JsonReader(new FileReader(jsonFile));
+		return gson.fromJson(reader, Config.class);
+	}
+
+	public String getPathFile() {
+		return pathFile;
+	}
+
+	public void setPathFile(String pathFile) {
+		this.pathFile = pathFile;
+	}
 }
