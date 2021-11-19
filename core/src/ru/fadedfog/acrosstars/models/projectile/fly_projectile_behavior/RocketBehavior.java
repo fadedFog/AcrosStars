6c4b7aef92403ee3420fb4c2df86e60e2a394259
Ilya Bastrakov <i.bastrakov.00@gmail.com>
2021-11-19 06:56:03 +0300

    Changed flyProjectile() for rokcet

--- core/src/ru/fadedfog/acrosstars/models/projectile/fly_projectile_behavior/RocketBehavior.java
+++ core/src/ru/fadedfog/acrosstars/models/projectile/fly_projectile_behavior/RocketBehavior.java
@@ -2,6 +2,7 @@ package ru.fadedfog.acrosstars.models.projectile.fly_projectile_behavior;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Polygon;
+import com.badlogic.gdx.math.Vector2;
 
 import ru.fadedfog.acrosstars.models.projectile.Projectile;
 
@@ -10,22 +11,24 @@ public class RocketBehavior implements FlyBehaviorProjectile {
 	private long startTime;
 	
 	@Override
-	public Polygon flyProjectile(Projectile projectile, float speedY) {
+	public Polygon flyProjectile(Projectile projectile, float speed) {
 		Polygon areaProjectile = projectile.getAreaObject();
+		float x = areaProjectile.getX();
+		float y = areaProjectile.getY();
+		Vector2 vectorDirection = projectile.getVectorDirection();
 		if (startTime == 0) {
 			startTime = System.currentTimeMillis();
 		}
 		
-		float speedChanged = speedY;
+		float speedChanged = speed;
 		if (System.currentTimeMillis() - startTime < 500l) {
 			speedChanged /= 2.5f;
 		}
 		
-		float x = areaProjectile.getX();
-		float y = areaProjectile.getY();
-		y += speedChanged * Gdx.graphics.getDeltaTime();
-		areaProjectile.setPosition(x, y);
+		x += vectorDirection.x * speedChanged * Gdx.graphics.getDeltaTime();
+		y += vectorDirection.y * speedChanged * Gdx.graphics.getDeltaTime();
 		
+		areaProjectile.setPosition(x, y);
 		return areaProjectile;
 	}
 
