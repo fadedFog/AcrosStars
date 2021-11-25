d4484da50d92de4a5ba6ed040a5529ca2f9dae42
Ilya Bastrakov <i.bastrakov.00@gmail.com>
2021-11-25 21:13:24 +0300

    Add shooting a bastion ship

--- core/src/ru/fadedfog/acrosstars/models/enemies/attack_behavior/AttackAimAndShoot.java
+++ core/src/ru/fadedfog/acrosstars/models/enemies/attack_behavior/AttackAimAndShoot.java
@@ -1,11 +1,13 @@
 package ru.fadedfog.acrosstars.models.enemies.attack_behavior;
 
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
 
 import ru.fadedfog.acrosstars.AcrosStarsGame;
 import ru.fadedfog.acrosstars.models.SpaceShip;
 import ru.fadedfog.acrosstars.models.cannon.Cannon;
 import ru.fadedfog.acrosstars.models.enemies.EnemyShip;
+import ru.fadedfog.acrosstars.models.projectile.Projectile;
 
 public class AttackAimAndShoot implements AttackBehavior {
 	private final float STRAIGHT_CORNE = 90f;
@@ -21,21 +23,31 @@ public class AttackAimAndShoot implements AttackBehavior {
 
 	@Override
 	public void attack() {
-		aiming();
-	}
-	
-	private void aiming() {
 		SpaceShip spaceShip = game.getSpaceShip();
-		Cannon cannon = ship.getCannon();
+		Cannon enemyCannon = ship.getCannon();
 		float xShip = spaceShip.getCenterPosition().x;
 		float yShip = spaceShip.getCenterPosition().y;
-		float xCannon = cannon.getX();
-		float yCannon = cannon.getY();
+		float xCannon = enemyCannon.getX();
+		float yCannon = enemyCannon.getY();
+		Vector2 vecShip = new Vector2(xShip, yShip);
+		Vector2 vecCannon = new Vector2(xCannon, yCannon);
 		
-		float angle = MathUtils.radiansToDegrees * MathUtils.atan2(yShip - yCannon, xShip - xCannon);
+		enemyCannon.getAreaObject().setRotation(aiming(vecShip, vecCannon));
+		shoot(vecShip, vecCannon, enemyCannon);
+	}
+	
+	private float aiming(Vector2 vecShip, Vector2 vecCannon) {
+		float angle = MathUtils.radiansToDegrees * MathUtils.atan2(vecShip.y - vecCannon.y, vecShip.x - vecCannon.x);
 		angle -= STRAIGHT_CORNE;
-		
-		cannon.getAreaObject().setRotation(angle);
+		return angle;
+	}
+	
+	private void shoot(Vector2 vecShip, Vector2 vecCannon, Cannon enemyCannon) {
+		enemyCannon.shoot();
+		int lastId = enemyCannon.getProjectilesOut().size() - 1;
+		Projectile lastProjectile = enemyCannon.getProjectilesOut().get(lastId);
+		Vector2 vecDirection = vecShip.sub(vecCannon).nor();
+		lastProjectile.setVectorDirection(vecDirection);
 	}
 	
 	@Override
